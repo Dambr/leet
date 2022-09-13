cb3de797178f73433c82174c232f1ffab56fbc32
dambr <dambr@localhost>
2022-09-13 21:08:40 +0300

    Решена задача 36

---
+++ leet/test/leet/task36/SolutionTest.java
@@ -0,0 +1,57 @@
+package leet.task36;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+	private Solution solution;
+	
+	@BeforeEach
+	void setUp() {
+		this.solution = new Solution();
+	}
+
+	@Test
+	void ex1Test() {
+		// Given
+		char[][] board = 
+				{{'5','3','.','.','7','.','.','.','.'}
+				,{'6','.','.','1','9','5','.','.','.'}
+				,{'.','9','8','.','.','.','.','6','.'}
+				,{'8','.','.','.','6','.','.','.','3'}
+				,{'4','.','.','8','.','3','.','.','1'}
+				,{'7','.','.','.','2','.','.','.','6'}
+				,{'.','6','.','.','.','.','2','8','.'}
+				,{'.','.','.','4','1','9','.','.','5'}
+				,{'.','.','.','.','8','.','.','7','9'}};
+		
+		// Then
+		boolean expected = true;
+		boolean actual = solution.isValidSudoku(board);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void ex2Test() {
+		// Given
+		char[][] board = 
+				{{'8','3','.','.','7','.','.','.','.'}
+				,{'6','.','.','1','9','5','.','.','.'}
+				,{'.','9','8','.','.','.','.','6','.'}
+				,{'8','.','.','.','6','.','.','.','3'}
+				,{'4','.','.','8','.','3','.','.','1'}
+				,{'7','.','.','.','2','.','.','.','6'}
+				,{'.','6','.','.','.','.','2','8','.'}
+				,{'.','.','.','4','1','9','.','.','5'}
+				,{'.','.','.','.','8','.','.','7','9'}};
+		
+		// Then
+		boolean expected = false;
+		boolean actual = solution.isValidSudoku(board);
+		assertEquals(expected, actual);
+	}
+
+}
